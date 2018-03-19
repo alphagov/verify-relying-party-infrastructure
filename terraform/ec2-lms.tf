a5139c12b41021444ed4aee55d2c96254c3e6e9f
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-03-19 11:14:00 +0000

    TT-2011: Cloud-init for local matching service example
    Co-authored-by: Rob Larsen <rob.larsen@digital.cabinet-office.gov.uk>

--- terraform/ec2-lms.tf
+++ terraform/ec2-lms.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "verify_connect_lms" {
   disable_api_termination     = false
   associate_public_ip_address = false
   source_dest_check           = false
-  user_data                   = "${file("user-data.sh")}"
+  user_data                   = "${file("lms/cloud-init.yaml")}"
 
   root_block_device {
     volume_size           = "50"
